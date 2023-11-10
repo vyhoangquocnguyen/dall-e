f5c10d31c2ccae4e4ddf7cba3ae44a687f395a56
Vy Nguyễn <36269757+vyhoangquocnguyen@users.noreply.github.com>
2023-11-09 20:42:51 -0500

    first commit

---
+++ client/src/utils/index.js
@@ -0,0 +1,14 @@
+import FileSaver from 'file-saver';
+import { surpriseMePrompts } from '../constants';
+
+export function getRandomPrompt(prompt) {
+  const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length);
+  const randomPromt = surpriseMePrompts[randomIndex];
+
+  if (randomPromt === prompt) return getRandomPrompt(prompt);
+  return randomPromt;
+}
+
+export async function downloadImage(_id, photo) {
+  FileSaver.saveAs(photo, `download-${_id}.jpg`);
+}
